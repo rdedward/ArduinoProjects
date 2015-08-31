d23201eab59258efa482307c80d34af89afeb56f
Ryan <edwardsr75@gmail.com>
2015-08-31 14:16:22 -0400

    Adding everything that wan't already added. Honestly can't remember how much of this is important

--- Vision/buggyMotion.py
+++ Vision/buggyMotion.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+import sys
+import serial
+import time
 
 def diffImg(t0, t1, t2):
   d1 = cv2.absdiff(t2, t1)
@@ -8,8 +11,55 @@ def diffImg(t0, t1, t2):
 
 
 def diffImg2(t0, t2):
+  #print "t0 shape: ", t0.shape
   d1 = cv2.absdiff(t2, t0)
+  #print "d1 shape: ", d1.shape
   return d1
+
+def vis_to_ard(locx,locy):
+    servo_midy = 95 #neutral y and x 
+    servo_midx = 95
+
+    servo_miny = 70
+    servo_maxy = 115
+
+    servo_minx = 70
+    servo_maxx = 130
+
+
+    locx = locx-320/2 #difference from center
+    locy = locy-240/2
+
+    width = 130-70
+    height = 115-70
+    newx = width/2*locx/(320/2)+servo_midx #percent of difference from center*center
+    newy = height/2*locy/(240/2)+servo_midy
+
+
+    if(newx < servo_minx):
+        newx = servo_minx
+        print "exceeded minx"
+
+
+    if(newy < servo_miny):
+        newy = servo_miny
+        print "exceeded miny"
+
+    if(newx > servo_maxx):
+        newx = servo_maxx
+        print "exceeded maxx"
+
+    if(newy > servo_maxy):
+        newy = servo_maxy
+        print "exceeded maxy"
+    
+    sendValue(newx,newy)
+
+
+def sendValue(x,y):
+    ser = serial.Serial('/dev/ttyACM1',9600)
+    ser.write(str(int(x))+'\n'+str(int(y))+'\n')
+    print "sent value"
 
 
 def detectBlobs(d1):
@@ -36,20 +86,24 @@ def detectBlobs(d1):
           box = np.int0(box)
 
           x,y,w,h = cv2.boundingRect(cnt)
-          avg_x += x
-          avg_y += y
+          avg_x += x+w/2
+          avg_y += y+h/2
           count += 1
           if (w*h >max_w*max_h):
               max_w, max_h, max_x, max_y = w, h, x, y
-      cv2.rectangle(d1,(max_x,max_y),(max_x+max_w,max_y+max_h),(255,255,255),2)
-      cv2.circle(d1, (int(avg_x/count), int(avg_y/count)), 15, (255,255,255), -1)
+      d1 = cv2.cvtColor(d1, cv2.COLOR_GRAY2BGR,d1, 3)
+      cv2.rectangle(d1,(max_x,max_y),(max_x+max_w,max_y+max_h),(0,255,0),2)
+      cv2.circle(d1, (int(avg_x/count), int(avg_y/count)), 15, (255,0,0), -1)
+      cv2.circle(d1, (int(max_x+max_w/2), int(max_y+max_h/2)), 15, (0,255,0), -1) #good
+      vis_to_ard(int(max_x+max_w/2),int(max_y+max_h/2))
+      
   return d1
 
 def treatImg(d1):
   d1 = cv2.medianBlur(d1, 5)
   
   adaptive_thresh_blocksize = 31
-  adaptive_thresh = 10 #10 for okay detection
+  adaptive_thresh = 9 #10 for okay detection
   d1 = cv2.adaptiveThreshold(d1,
                         255,
                         cv2.ADAPTIVE_THRESH_MEAN_C,
@@ -61,15 +115,14 @@ def treatImg(d1):
   kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
   
   d1 = cv2.erode(d1, kernel, iterations = 1)
-  d1 = cv2.dilate(d1, kernel, iterations = 5)
-  print d1.shape
+  d1 = cv2.dilate(d1, kernel, iterations = 12)
+  print "shape: ",d1.shape
   return d1
 
 old = []
 
 
-cam = cv2.VideoCapture(0)
-
+cam = cv2.VideoCapture(1)
 
 winName = "Movement Indicator"
 cv2.namedWindow(winName, cv2.CV_WINDOW_AUTOSIZE)
@@ -84,7 +137,7 @@ while len(old)< 20:
   old.append(template)
 
 while True:
-  #cv2.imshow( winName, diffImg(t_minus, t, t_plus) )
+  #cv2.imshow( winName, diffImg2(t_minus, t_plus) )
   cv2.imshow( winName, detectBlobs(treatImg(diffImg2(t_minus, t_plus))) )
   #cv2.imshow( winName, treatImg(diffImg2(t_minus, t_plus)) )
 
